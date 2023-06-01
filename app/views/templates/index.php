be293eef1d5df4572ecfa5da1a484466727061f5
Florent Lecocq <lecocqflorent@outlook.fr>
2023-06-01 15:49:19 +0200

    ajout des partials + connexion

---
+++ app/views/templates/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">  
+    <head>
+        <?php include '../app/views/template/partials/_head.php'?>
+    </head>
+    <body class="bg-gray-900 text-white font-sans leading-normal tracking-normal">
+        <?php include '../app/views/template/partials/_header.php'?>
+        <?php include '../app/views/template/partials/_main.php'?>
+        <?php include '../app/views/template/partials/_footer.php'?>
+    </body>
+</html>
---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,6 @@
+<div class="container mx-auto flex flex-wrap pt-4 pb-12 text-white">
+    <?php include '../app/views/template/partials/_aside.php'?>
+    <main class="w-full md:w-3/4 p-3">
+        <?php echo $content?>
+    </main>
+</div>
